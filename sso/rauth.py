62c4318d6240335db1016879f8792a0d5cf2d444
MLC <gymkhana.iitb@gmail.com>
2020-01-24 21:52:34 +0000

    Update and create users from RemoteBackend

--- sso/rauth.py
+++ sso/rauth.py
@@ -1,4 +1,42 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import RemoteUserBackend
+from django.contrib.auth.middleware import RemoteUserMiddleware
+from account.models import UserProfile
+
+class RemoteUserCustomMiddleware(RemoteUserMiddleware):
+    def process_request(self, request):
+        res = super(RemoteUserCustomMiddleware, self).process_request(request)
+
+        # Configure from request
+        def set_key(obj, key, valkey):
+            if valkey in request.META and getattr(obj, key) != request.META[valkey]:
+                setattr(obj, key, request.META[valkey])
+                set_key.changed = True
+
+        # Track changes
+        set_key.changed = False
+
+        # Update object
+        user = request.user
+        if request.user.is_authenticated():
+            set_key(user, 'email', 'OIDC_CLAIM_email')
+            set_key(user, 'first_name', 'OIDC_CLAIM_givenName')
+            set_key(user, 'last_name', 'OIDC_CLAIM_sn')
+
+            if hasattr(request.user, 'userprofile'):
+                profile = request.user.userprofile
+            else:
+                profile = UserProfile.objects.create(user=user)
+
+            set_key(profile, 'roll_number', 'OIDC_CLAIM_employeeNumber')
+            set_key(profile, 'type', 'OIDC_CLAIM_employeeType')
+
+            if set_key.changed:
+                user.save()
+                profile.save()
+
+        return res
+
 class RemoteUserCustomBackend(RemoteUserBackend):
-    create_unknown_user = False
+    create_unknown_user = True
 
