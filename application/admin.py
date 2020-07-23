544f1e29e16ea5caadaa30c569abde1e255acdda
MLC <gymkhana.iitb@gmail.com>
2020-07-23 06:04:04 +0000

    Make raw_id_fields

--- application/admin.py
+++ application/admin.py
@@ -8,6 +8,7 @@ class ApplicationAdmin(SimpleHistoryAdmin):
     list_display = ['id', 'name', 'user', 'total_users', 'created_on', 'modified_on', 'is_anonymous']
     list_filter = ['is_anonymous', ]
     search_fields = ['name', 'user__username', 'user__first_name', ]
+    raw_id_fields = ('user',)
 
     def total_users(self, obj):
         return obj.get_user_count()
--- user_resource/admin.py
+++ user_resource/admin.py
@@ -8,12 +8,14 @@ class InstituteAddressAdmin(SimpleHistoryAdmin):
     list_display = ['id', 'user', 'hostel', 'room']
     list_filter = ['hostel']
     search_fields = ['user__username', 'user__first_name', 'room']
+    raw_id_fields = ('user',)
 
 
 class ProgramAdmin(SimpleHistoryAdmin):
     list_display = ['id', 'user', 'department', 'join_year', 'graduation_year', 'degree']
     list_filter = ['department', 'join_year', 'graduation_year', 'degree', ]
     search_fields = ['user__username', 'user__first_name', ]
+    raw_id_fields = ('user',)
 
     class Media:
         js = ['admin/js/list_filter_collapse.js', ]
@@ -22,17 +24,20 @@ class ProgramAdmin(SimpleHistoryAdmin):
 class ContactNumberAdmin(SimpleHistoryAdmin):
     list_display = ['id', 'user', 'number']
     search_fields = ['user__username', 'user__first_name', 'number', ]
+    raw_id_fields = ('user',)
 
 
 class SecondaryEmailAdmin(SimpleHistoryAdmin):
     list_display = ['id', 'user', 'email']
     search_fields = ['user__username', 'user__first_name', 'email', ]
+    raw_id_fields = ('user',)
 
 
 class SentMessageAdmin(SimpleHistoryAdmin):
     list_display = ['id', 'message_id', 'sender', 'user', 'status', 'created', ]
     list_filter = ['status', 'sender', ]
     search_fields = ['sender__id', 'sender__name', 'user__username', 'user__first_name', 'message_id', ]
+    raw_id_fields = ('user','sender')
 
     class Media:
         js = ['admin/js/list_filter_collapse.js', ]
